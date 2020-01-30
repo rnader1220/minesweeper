9b1c453c9d4aeefa94ac6eadedc4233e88c5140b
= <=rnader@dyn-it.com>
2020-01-30 09:00:25 -0500

    upd doc

--- public/minesweeper.js
+++ public/minesweeper.js
@@ -1,9 +1,120 @@
 var minesweeper = (function($, undefined) {
 
+	var session_id = null;
 
-    return {
-		load: load,
-		set_config: set_config
+	var initialize = function() {
+		session_id = null;
+		setSessionState();
+	}
+
+	var register = function() {
+		$('#login').hide();
+		$.ajax({
+			url: '/register',
+			cache: false,
+		})
+		.done(function(html) {
+			$('#login').html(html).fadeIn(300);
+			$('#login form').on('submit', function (e) {
+				e.preventDefault();
+				if ($(this).valid()) $.ajax({
+					type: "POST",
+					url: '/register',
+					data: $(this).serialize(),
+				})
+					.done(function (resp) {
+
+						$('#login').fadeOut(300);
+						session_id = resp;
+						setTimeout(function () {
+							setSessionState();
+						}, 300);
+					})
+					.fail(function (message) {
+						alert(message);
+					});
+			}).validate();
+		});
+	};
+
+	var login = function() {
+		$('#login').hide();
+
+		$.ajax({
+			url: '/login',
+			cache: false,
+		})
+		.done(function(html) {
+			$('#login').html(html).fadeIn(300);
+
+			$('#login form').on('submit', function (e) {
+				e.preventDefault();
+				if ($(this).valid()) $.ajax({
+					type: "POST",
+					url: '/login',
+					data: $(this).serialize(),
+				})
+					.done(function (resp) {
+						$('#login').fadeOut(300);
+						session_id = resp.session_id;
+						setTimeout(function () {
+							setSessionState();
+						}, 300);
+
+					})
+					.fail(function (message) {
+						alert(message);
+					});
+			}).validate();
+		});
+	};
+
+	var setSessionState = function() {
+		// hide all menu items
+		$('[id^=menu-]').hide().off('click');
+		if(session_id == null) {
+			$('#menu-login').on('click', login).show();
+			$('#menu-register').on('click', register).show();
+			// show login
+			// show register
+		} else {
+			$('#menu-start').on('click', start).show();
+			$('#menu-history').on('click', list).show();
+			$('#menu-logout').on('click', logout).show();
+		}
+
+	}
+	var list = function() {
+		// load into tables grid
+	}
+
+	var select = function() {
+		// get game id value
+		// request game state
+		// renderGame(gameMap)	
+	}
+
+	var start = function() {
+		// request new game ui screen	
+		// on save ...	
+		// renderGame(gameMap)	
+	}
+
+	var renderGame = function(gameMap) {
+		// gameMap is JSON
+		// rows, columns, cells
+		// set status
+		// can I just draw a table??
+	}
+
+	var logout = function() {
+		session_id = null;
+		setSessionState();
+	}
+
+
+	return {
+		initialize: initialize
 	};
 })(jQuery);
 
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -16,56 +16,64 @@
                 <div class='help'>
                     API Calls
                     <ul>
-                        <li>(get) /register<br>
-                        <strong>accepts:</strong> nothing<br>
-                        <strong>returns:</strong> registration form HTML<br>
-                        </li>
-                        <li>(post) /register
-                        <strong>accepts:</strong> post of registration form<br>
-                        <strong>returns on success:</strong> session id JSON<br>
-                        <strong>returns on failure:</strong> failure reason HTML<br>
+                        <li style = 'color:gray'>(get) /register<br>
+                            create a new user account<br>
+                            <strong>accepts:</strong> nothing<br>
+                            <strong>returns:</strong> registration form HTML<br>
                         </li>
 
-
-                        <li>(get) /login<br>
-                        <strong>accepts:</strong> nothing<br>
-                        <strong>returns:</strong> registration form HTML<br>
+                        <li style = 'color:gray'>(post) /register
+                            create a new user account, start user session<br>
+                            <strong>accepts:</strong> post of registration form<br>
+                            <strong>returns on success:</strong> session id JSON<br>
+                            <strong>returns on failure:</strong> failure reason HTML<br>
                         </li>
 
-                        <li>(post) /login<br>
+                        <li style = 'color:gray'>(get) /login<br>
+                            existing user login<br>
+                            <strong>accepts:</strong> nothing<br>
+                            <strong>returns:</strong> registration form HTML<br>
+                        </li>
+
+                        <li style = 'color:gray'>(post) /login<br>
+                            start user session<br>
                             <strong>accepts:</strong> post of login form<br>
                             <strong>returns on success:</strong> session id JSON<br>
                             <strong>returns on failure:</strong> failure reason HTML<br>
                         </li>
 
-                        <li>(get) /start<br>
-                        <strong>accepts:</strong> session id<br>
-                        <strong>returns:</strong> game_config form HTML<br>
+                        <li style = 'color:gray'>(get) /start<br>
+                            start new user game<br>
+                            <strong>accepts:</strong> session id<br>
+                            <strong>returns:</strong> game_config form HTML<br>
                         </li>
-                        <li>(post) /start<br>
+                        <li style = 'color:gray'>(post) /start<br>
+                            start new user game<br>
                             <strong>accepts:</strong> post of game_config form<br>
                             <strong>returns on success:</strong> game id JSON<br>
                             <strong>returns on failure:</strong> failure reason HTML<br>
                         </li>
-                        <li>(get) /history<br>
+                        <li style = 'color:gray'>(get) /list<br>
+                            show user games<br>
                             <strong>accepts:</strong> session id<br>
                             <strong>returns on success:</strong> user game list JSON<br>
                             <strong>returns on failure:</strong> failure reason HTML<br>
-                        </li>
+                        </li style = 'color:gray'>
 
-                        <li>(get) /resume<br>
+                        <li style = 'color:gray'>(get) /resume<br>
+                            resume incomplete user game<br>
                             <strong>accepts:</strong> game id<br>
                             <strong>returns on success:</strong> game status map JSON<br>
                             <strong>returns on failure:</strong> failure reason HTML<br>
                         </li>
 
-                        <li>(get) /click<br>
+                        <li style = 'color:gray'>(get) /click<br>
                             location click for exposure<br>
                             <strong>accepts:</strong> coordinate pair JSON<br>
                             <strong>returns on success:</strong> game status map JSON<br>
                             <strong>returns on failure:</strong> failure reason HTML<br>
                         </li>
-                        <li>(get) /flag<br>
+                        <li style = 'color:gray'>(get) /flag<br>
                             location click for flag toggle<br>
                             <strong>accepts:</strong> coordinate pair JSON<br>
                             <strong>returns on success:</strong> game status map JSON<br>
@@ -74,29 +82,40 @@
                     </ul>
                 </div>
                 <div class='help'>
-                    Game Status Map json:<br>
+                    Game List JSON:<br>
                     <pre><code>
                     {
                         id: game id,
                         columns: columns,
                         rows: rows,
                         mines: minecount,
-                        status: [1 = active, 0 = done],
+                        status: [0 = active, -1 = lost, 1 = won],
+                    }
+                    </code></pre>
+
+                    Game Status Map JSON:<br>
+                    <pre><code>
+                    {
+                        id: game id,
+                        columns: columns,
+                        rows: rows,
+                        mines: minecount,
+                        status: [0 = active, -1 = lost, 1 = won],
                         cells: {
-                            col = int (0 - columns-1), 
-                            row = int (0 - rows-1), 
-                            value = int (0::8, -1 = flagged, -2 = triggered, -3 = exposed), 
+                            col = int (0 to columns-1), 
+                            row = int (0 to rows-1), 
+                            value = char [' ', '1', '2', '3', '4', '5', '6', '7', '8', '&#9873;', '&#10040;' , '&#10042;'), 
                         }
                     }
                     </code></pre>
                     notes:  
                     <ul>
                         <li>cells lists shows cells that have been selected.</li>
-                        <li>positive value shows how many mines are nearby.</li>
-                        <li>0 means not adjacent mines are found</li>
-                        <li>-1 means user flagged the cell. (flag icon) &#9873; </li>
-                        <li>-2 means user triggered that mine (red mine recommended) <span style='color:red'>&#10041;</span> </li>
-                        <li>-3 means all mines are exposed, and game has ended in loss. &#10041;</li> 
+                        <li>'1' (etc) positive values shows how many mines are nearby.</li>
+                        <li>' ' means no adjacent mines are found</li>
+                        <li> '&#9873;' means user flagged the cell. (flag icon)  </li>
+                        <li> '&#10040;' means user triggered that mine (red mine recommended)  </li>
+                        <li> '&#10042;' means all mines are exposed, and game has ended in loss.</li> 
                     </ul>
 
             </div>
