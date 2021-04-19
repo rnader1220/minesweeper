a508132d41d145e5bfbc57151d6464f1ccf8db15
Richard Nader <rnader@dyn-it.com>
2021-04-19 12:06:22 +0000

    background image, swap default pages

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,124 +1,152 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Minesweeper API</title>
+		<title>Richard's Minesweeper</title>
+		<link href="https://fonts.googleapis.com/css?family=Permanent+Marker&display=swap" rel="stylesheet">
+		<link href="https://fonts.googleapis.com/css2?family=Exo+2:wght@800&display=swap" rel="stylesheet">
+		<script
+			 src="https://code.jquery.com/jquery-3.4.1.min.js"
+			 integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo="
+			 crossorigin="anonymous"></script>
+			 <script type="text/javascript" src="minesweeper.js?v=0.9.0.9"></script>
 
+			 <script
+			 src="https://cdn.jsdelivr.net/npm/jquery-validation@1.19.1/dist/jquery.validate.min.js"></script>
 
-    </head>
-    <body>
-            <div class="content">
-                <div class="title m-b-md">
-                    Minesweeper API Help
-                </div>
-                <div class='help'>
-                    API Calls
-                    <ul>
-                        <li style = 'color:gray'>(get) /register<br>
-                            create a new user account<br>
-                            <strong>accepts:</strong> nothing<br>
-                            <strong>returns:</strong> registration form HTML<br>
-                        </li>
+		<link rel="stylesheet" type="text/css" href="minesweeper.css?v=0.9.0.9">
 
-                        <li style = 'color:gray'>(post) /register
-                            create a new user account, start user session<br>
-                            <strong>accepts:</strong> post of registration form<br>
-                            <strong>returns on success:</strong> session id JSON<br>
-                            <strong>returns on failure:</strong> failure reason HTML<br>
-                        </li>
+	</head>
+	<body style="
+        background: black url('/images/minesweeper.jpg');
+        background-size: cover;
+        background-repeat: no-repeat;
+        ">
+		<div class="menu-main">
+			<a id='menu-login'>Login</a>
+			<a id='menu-register'>Register</a>
+			<a id='menu-start'>New Game</a>
+			<a id='menu-logout'>Good Bye!</a>
+		</div>
 
-                        <li style = 'color:gray'>(get) /login<br>
-                            existing user login<br>
-                            <strong>accepts:</strong> nothing<br>
-                            <strong>returns:</strong> registration form HTML<br>
-                        </li>
+		<div id='title'>
+			<div class="title m-b-md">
+			Death From Below!
+			</div>
+		</div>
+		<div id='login' class="content">
+			<div>
+				<div>
+					<h4>Back for more punishment?</h4>
+				</div>
+				<form>
+					<div>
+						<label for="email">E-Mail Address</label>
+						<div>
+							<input id="email" type="email"  name="email" required>
+						</div>
+					</div>
 
-                        <li style = 'color:gray'>(post) /login<br>
-                            start user session<br>
-                            <strong>accepts:</strong> post of login form<br>
-                            <strong>returns on success:</strong> session id JSON<br>
-                            <strong>returns on failure:</strong> failure reason HTML<br>
-                        </li>
+					<div>
+						<label for="password">Password</label>
+						<div>
+							<input id="password" type="password"  name="password" required>
 
-                        <li style = 'color:gray'>(get) /start<br>
-                            start new user game<br>
-                            <strong>accepts:</strong> session id<br>
-                            <strong>returns:</strong> game_config form HTML<br>
-                        </li>
-                        <li style = 'color:gray'>(post) /start<br>
-                            start new user game<br>
-                            <strong>accepts:</strong> post of game_config form<br>
-                            <strong>returns on success:</strong> game id JSON<br>
-                            <strong>returns on failure:</strong> failure reason HTML<br>
-                        </li>
-                        <li style = 'color:gray'>(get) /list<br>
-                            show user games<br>
-                            <strong>accepts:</strong> session id<br>
-                            <strong>returns on success:</strong> user game list JSON<br>
-                            <strong>returns on failure:</strong> failure reason HTML<br>
-                        </li style = 'color:gray'>
+						</div>
+					</div>
+					<br>
+					<div>
+						<div>
+							<button type="submit">
+								Log in
+							</button>
+						</div>
+					</div>
+				</form>
+			</div>
+		</div>
 
-                        <li style = 'color:gray'>(get) /resume<br>
-                            resume incomplete user game<br>
-                            <strong>accepts:</strong> game id<br>
-                            <strong>returns on success:</strong> game status map JSON<br>
-                            <strong>returns on failure:</strong> failure reason HTML<br>
-                        </li>
+		<div id='register' class='content'>
+			<div>
+				<div>
+					<h4>Join us, if you dare!</h4>
+				</div>
+					<form>
+						<div>
+							<label for="name">Name</label>
+							<div>
+								<input id="name" type="text"  name="name" required autofocus>
+							</div>
+						</div>
 
-                        <li style = 'color:gray'>(get) /click<br>
-                            location click for exposure<br>
-                            <strong>accepts:</strong> coordinate pair JSON<br>
-                            <strong>returns on success:</strong> game status map JSON<br>
-                            <strong>returns on failure:</strong> failure reason HTML<br>
-                        </li>
-                        <li style = 'color:gray'>(get) /flag<br>
-                            location click for flag toggle<br>
-                            <strong>accepts:</strong> coordinate pair JSON<br>
-                            <strong>returns on success:</strong> game status map JSON<br>
-                            <strong>returns on failure:</strong> failure reason HTML<br>
-                        </li>
-                    </ul>
-                </div>
-                <div class='help'>
-                    Game List JSON:<br>
-                    <pre><code>
-                    {
-                        id: game id,
-                        columns: columns,
-                        rows: rows,
-                        mines: minecount,
-                        status: [0 = active, -1 = lost, 1 = won],
-                    }
-                    </code></pre>
+						<div>
+							<label for="email">E-Mail Address</label>
+							<div>
+								<input id="email" type="email"  name="email" required>
+							</div>
+						</div>
 
-                    Game Status Map JSON:<br>
-                    <pre><code>
-                    {
-                        id: game id,
-                        columns: columns,
-                        rows: rows,
-                        mines: minecount,
-                        status: [0 = active, -1 = lost, 1 = won],
-                        cells: {
-                            col = int (0 to columns-1), 
-                            row = int (0 to rows-1), 
-                            value = char [' ', '1', '2', '3', '4', '5', '6', '7', '8', '&#9873;', '&#10040;' , '&#10042;'), 
-                        }
-                    }
-                    </code></pre>
-                    notes:  
-                    <ul>
-                        <li>cells lists shows cells that have been selected.</li>
-                        <li>'1' (etc) positive values shows how many mines are nearby.</li>
-                        <li>' ' means no adjacent mines are found</li>
-                        <li> '&#9873;' means user flagged the cell. (flag icon)  </li>
-                        <li> '&#10040;' means user triggered that mine (red mine recommended)  </li>
-                        <li> '&#10042;' means all mines are exposed, and game has ended in loss.</li> 
-                    </ul>
+						<div>
+							<label for="password">Password</label>
+							<div>
+								<input id="password" type="password"  name="password" required>
 
-            </div>
-        </div>
-    </body>
+							</div>
+						</div>
+
+						<div>
+							<label for="c_password">Confirm Password</label>
+							<div>
+								<input id="c_password" type="password"  name="c_password" required>
+							</div>
+						</div>
+						<br>
+						<div>
+							<div>
+								<button type="submit">
+									Register
+								</button>
+							</div>
+						</div>
+					</form>
+				</div>
+			</div>
+
+		</div>
+		<div id='start' class="content">
+			<form>
+				<div>
+					<h4>Challenge is why we're here!</h4>
+				</div>
+
+			<ul style='list-style:none'>
+				<li><input id='size', name='size' type='radio', value = 10> Small Game (8x8, 10 mines)</li>
+				<li><input id='size', name='size' type='radio', value = 40> Medium Game (16x16, 40 mines)</li>
+				<li><input id='size', name='size' type='radio', value = 99> Large Game (30x16, 99 mines)</li>
+
+			</ul>
+			<button type="submit">
+				Lets Go
+			</button>
+
+		</div>
+		<div id='list' class="content">
+		</div>
+		<div id='game' class="content">
+			<div style='text-align:center'>
+			<table  id='game_table' style='border: collapse; margin-left:auto; margin-right:auto' cellspacing=0 cellborder=0>
+			</table>
+			</div>
+		</div>
+		<div id='result' class="content"></div>
+	</body>
 </html>
+
+<script type="text/javascript">
+	$(document).ready(function(){
+		minesweeper.initialize();
+	});
+	</script>
+
